39b0c129c1251ef8406eb10af837acbc2df3d9c9
foxxstore <rakairwan81@gmail.com>
2024-01-30 13:57:21 +0700

    dom

--- javascript-dom/object.js
+++ javascript-dom/object.js
@@ -1,11 +1,25 @@
-var siswa =  new Object();
-siswa.namaDepan = "asep"
-siswa.namaBelakang = "gerung"
-siswa.alamat = "bandung"
-siswa.namaLengkap = function() {
-    return this.namaDepan + " " + this.namaBelakang;
+//object
+var siswa = new Object();
+siswa.namaDepan = "asep";
+siswa.namaBelakang = "gerung";
+siswa.alamat = "bandung";
+siswa.namaLengkap = function () {
+  return this.namaDepan + " " + this.namaBelakang;
 };
 
-alert ("Nama :" + siswa.namaLengkap());
+//object JSON
+var siswa2 = {
+  namaDepan: "Raka",
+  namaBelakang: "adiputrea              ",
+  alamat: "bandung",
+  namaLengkap: function () {
+    return this.namaDepan + " " + this.namaBelakang;
+  },
+};
 
-//functiom nama lengkap
+alert("Nama :" + siswa.namaLengkap());
+alert("Nama : " + siswa2.namaLengkap());
+
+// function namaLengkap() {
+
+// // }
